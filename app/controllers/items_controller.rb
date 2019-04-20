eb1b7a20f4b0fca43bfcc2413aa586e4f44b9d52
Claire Fahy <zeebear21@gmail.com>
2019-04-19 21:47:19 -0300

    index show new create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,43 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: %i[show edit update delete]
+
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    redirect_to root_path if @item.save
+
+    render 'new'
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
 end
