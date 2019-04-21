5c6452c8b2b704480c172e831d4dc1cd06bbb808
Claire Fahy <zeebear21@gmail.com>
2019-04-21 19:45:58 -0300

    add complete methods and route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :find_item, only: %i[show edit update delete]
+  before_action :find_item, only: %i[show edit update delete complete]
 
   def index
     if user_signed_in?
@@ -32,6 +32,11 @@ class ItemsController < ApplicationController
 
   def delete
     @item.destroy
+    redirect_to root_path
+  end
+
+  def complete
+    @item.update_attribute(:completed_at, Time.now)
     redirect_to root_path
   end
 
